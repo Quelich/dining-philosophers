28c9d227aff972a37339d36757e4dcb66d5969bb
emre <emre@Ubuntu22.04.myguest.virtualbox.org>
2023-01-06 15:55:55 +0300

    add mutex for shared data

--- phsp.c
+++ phsp.c
@@ -29,139 +29,49 @@ int max_dine;
 int min_dine;
 int dst_type;
 int dine_num;
-pthread_mutex_t mutex;
+pthread_mutex_t chopsticks[MAX_PHSP];
 sem_t sems[MAX_PHSP];
 
-// int get_mean(int min, int max)
-// {
-//     return (min + max) / 2;
-// }
 
-// //todo: check the calculation again
-// double exprand(int min, int max)
-// {
-//     int lambda = get_mean(min, max);
-//     double u = (rand() % (max - min + 1)) + min;
-//     return -log(1 - u) / lambda;
-// }
-
-
-// //todo: check the calculation again
-// double unirand(int min, int max)
-// {
-//     return ((rand() / (1 + RAND_MAX)) * ((max - min) + 1)) + min;
-// }
-
-// get random think or dine time between specified boundaries
-// based on distribution type in milliseconds
-// int get_randtime(int min, int max)
-// {   
-//     int flag = 0;
-//     if (dst_type == EXPONENTIAL_DST)
-//     {   
-        
-//         while (flag != 1)
-//         {
-            
-//             int thinktime = (int)exprand(min, max);
-//             if (thinktime >= min && thinktime <= max)
-//             {   
-//                 flag = 1;
-//                 return thinktime;
-//             }
-//         }
-        
-//     }
-//     else if (dst_type == UNIFORM_DST)
-//     {
-//         while (flag != 1)
-//         {
-//             int thinktime = unirand(min, max);
-//             if (thinktime >= min && thinktime <= max)
-//             {
-//                 return thinktime;
-//             }
-//         }
-//     }
-    
-//     exit(EXIT_FAILURE);
-// }
-
-// double to_sec(int ms)
-// {
-//     return ms / 1000;
-// }
-
-// // get the distribution type as an integer
-// int get_dst(char *dst)
-// {
-//     if (strcmp(dst, EXPONENTIAL_LITERAL) == 0)
-//     {
-//         return EXPONENTIAL_DST;
-//     }
-//     else if (strcmp(dst, UNIFORM_LITERAL) == 0)
-//     {
-//         return UNIFORM_DST;
-//     }
-//     printf("Invalid distribution type\n");
-//     exit(EXIT_FAILURE);
-// }
-
-void test(int i)
-{   
-    int left = (i + num_phsp - 1) % num_phsp;
-    int right = (i + 1) % num_phsp;
-    if (state[i] == HUNGRY && state[left] != EATING && state[right] != EATING)
-    {
-        state[i] = EATING;
-        printf("Philosopher %d is picking up chopsticks %d and %d", i, left, i);
-        sem_post(&sems[i]);
-    }
+void think()
+{
+    sleep(2);
 }
 
-void pickup(int i)
+void eat()
 {
-    pthread_mutex_lock(&mutex);
-    state[i] = HUNGRY;
-    test(i);
-    pthread_mutex_unlock(&mutex);
-    sem_wait(&sems[i]);
-}
-
-void putdown(int i)
-{
-    int left = (i + num_phsp - 1) % num_phsp;
-    int right = (i + 1) % num_phsp;
-    pthread_mutex_lock(&mutex);
-    state[i] = THINKING;
-    test(left);
-    test(right);
-    pthread_mutex_unlock(&mutex);
-
+    sleep(2);
 }
 
 // philosopher thread
 void *philosopher(void *arg)
 {
-    int i;
+    int  i = *((int *)arg);
     int j = 0;
+    int left = i;
+    int right = (i + 1) % num_phsp;
     while (j < num_phsp)
     {
         i = *((int *)arg);
         // think
         printf("Philosopher %d is Thinking\n", i);
-        sleep(2); //TODO: change to get_randtime
-        // pickup chopstick[i] chopstick[(i + 1) % num_phsp]
+        think();
         printf("Philosopher %d is Hungry\n", i);
-        pickup(i);
+        // pickup(left);
+        // pickup(right);
+        pthread_mutex_lock(&chopsticks[left]);
+        pthread_mutex_lock(&chopsticks[right]);
         // eat
         printf("Philosopher %d is Eating\n", i);
-        sleep(1); //TODO: change to get_randtime
+        eat(); 
         printf("Philosopher %d is Done Eating\n", i);
-        // putdown chopstick[i] chopstick[(i + 1) % num_phsp]
-        putdown(i);
+        // putdown(left);
+        // putdown(right);
+        pthread_mutex_unlock(&chopsticks[left]);
+        pthread_mutex_unlock(&chopsticks[right]);
         j++;
     }
+    pthread_exit(NULL);
 }
 
  
@@ -169,43 +79,19 @@ int main(int argc, int *argv[])
 {
 
     num_phsp = MAX_PHSP;  /* number of philosophers */
-    // min_think = atoi(argv[2]); /* minimum thinking time for a philosopher */
-    // max_think = atoi(argv[3]); /* maximum thinking time for a philosopher */
-    // min_dine = atoi(argv[4]);  /* minimum dining time for a philosopher */
-    // max_dine = atoi(argv[5]);  /* maximum dining time for a philosopher */
-    // char *dst = argv[6];
-    // dst_type = get_dst(dst);
-    // dine_num = atoi(argv[7]); /* Each philosopher will dine num times */
     
-    // Highest 60 secs, lowest 1 ms
-    // if (min_think < MIN_CONDITION ||
-    //     max_think > MAX_CONDITION || 
-    //     min_dine < MIN_CONDITION  || 
-    //     max_dine > MAX_CONDITION)
-    // {
-    //     printf("Invalid high-low time bounds\n");
-    //     exit(EXIT_FAILURE);
-    // }
 
     // DEBUG
     printf("******************************\n");
     printf("num_phsp = %d\n", num_phsp);
-    // printf("min_think = %d ms\n", min_think);
-    // printf("max_think = %d ms\n", max_think);
-    // printf("min_dine = %d ms\n", min_dine);
-    // printf("max_dine = %d ms\n", max_dine);
-    // printf("dst = %s\n", dst);
-    // printf("num = %d\n", dine_num);
-    // printf("dst type = %d\n", dst_type);
     printf("******************************\n");
-
     
     int i;
-    int philosophers[num_phsp]; /* represents a philosopher */
-    pthread_t tids[num_phsp]; /* represents a philosopher as thread*/
+    int philosophers[num_phsp]; 
+    pthread_t tids[num_phsp]; /* philosopher threads */
     
-    // initialize the mutex
-    sem_init(&mutex, 0, 1);
+    // initialize the semaphores
+    sem_init(&chopsticks, 0, 1);
     
     /* 
        initialize the semaphores
@@ -214,7 +100,7 @@ int main(int argc, int *argv[])
     */
     for (i = 0; i < num_phsp; i++)
     {
-        sem_init(&sems[i], 0, 0);
+        pthread_mutex_init(&chopsticks[i], NULL);
     }
 
     // initialize the philosophers
